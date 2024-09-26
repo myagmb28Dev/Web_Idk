58c9698e2e020c587ec39da7b2056409f6556a33
myagmb28Dev <myagmb28s@gmail.com>
2024-09-26 15:40:22 +0900

    1st commit

---
+++ new-web/src/components/Login/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './Login.css';
+import pc from '../../images/pc.jpg';
+
+function Login() {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleClick = (e) => {
+    e.preventDefault(); 
+    console.log('아이디:', id);
+    console.log('비밀번호:', '*'.repeat(password.length)); 
+  };
+
+  return (
+    <div className='App'>
+    <div className="container">
+      <div className="form-section">
+        <h2>로그인</h2>
+        <form>
+          <label htmlFor="id">Id</label>
+          <input 
+            type="text" 
+            id="id" 
+            name="id" 
+            required 
+            placeholder='id'
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+          
+          <label htmlFor="password">Password</label>
+          <input 
+            type="password" 
+            id="password" 
+            name="password" 
+            required 
+            placeholder='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          
+          <button type="submit" onClick={handleClick}>Login</button>
+        </form>
+
+        <p>Sign Up <a href="/register">회원가입</a></p>
+      </div>
+    </div>
+    <img id='bg' src={pc} alt="login" />
+    </div>
+  );
+}
+
+export default Login;
